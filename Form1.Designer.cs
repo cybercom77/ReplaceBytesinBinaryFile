d2c36f13ada283e257cfb26016b40a13f4af7737
cybercom77 <44980155+cybercom77@users.noreply.github.com>
2022-03-19 17:05:29 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace ReplaceBytesinBinaryFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(622, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Patch file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(621, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(621, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Target File";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(722, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(103, 150);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(621, 124);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(89, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(613, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "This program patch EldenRing.exe to use fontx and msgx directory outside of data " +
+    "file";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Patch EldenRing exe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceBytesinBinaryFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        public static string ConvertHex(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hexstringsource= ConvertHex("6D00730067003A002F");
+            string hexstringtarget= ConvertHex("6D007300670078002F");
+            string temppatchfile = textBox2.Text + ".temp";
+            listBox1.Items.Add("Phase 1 - find 'm.s.g.:./'");
+            ReplaceTextInBinaryFile(textBox1.Text, temppatchfile, hexstringsource, hexstringtarget);
+            listBox1.Items.Add("Phase 1 FINISHED");
+
+            listBox1.Items.Add("Phase 2 - find 'm.s.g.b.n.d.......f.o.n.t.:./'");
+            string hexstringsource2 = ConvertHex("6D007300670062006E00640000000000000066006F006E0074003A002F");
+            string hexstringtarget2 = ConvertHex("6D007300670062006E00640000000000000066006F006E00740078002F");
+            ReplaceTextInBinaryFile(temppatchfile, textBox2.Text, hexstringsource2, hexstringtarget2);
+            listBox1.Items.Add("Phase 2 FINISHED");
+            File.Delete(temppatchfile);
+            button1.Enabled = false;
+            button3.Enabled = false;
+
+
+
+        }
+
+        private void ReplaceTextInBinaryFile(string originalFile, string outputFile, string searchTerm, string replaceTerm)
+        {
+            byte b;
+            //UpperCase bytes to search
+            byte[] searchBytes = Encoding.UTF8.GetBytes(searchTerm.ToUpper());
+            //LowerCase bytes to search
+            byte[] searchBytesLower = Encoding.UTF8.GetBytes(searchTerm.ToLower());
+            //Temporary bytes during found loop
+            byte[] bytesToAdd = new byte[searchBytes.Length];
+            //Search length
+            int searchBytesLength = searchBytes.Length;
+            //First Upper char
+            byte searchByte0 = searchBytes[0];
+            //First Lower char
+            byte searchByte0Lower = searchBytesLower[0];
+            //Replace with bytes
+            byte[] replaceBytes = Encoding.UTF8.GetBytes(replaceTerm);
+            int counter = 0;
+            using (FileStream inputStream = File.OpenRead(originalFile))
+            {
+                //input length
+                long srcLength = inputStream.Length;
+                using (BinaryReader inputReader = new BinaryReader(inputStream))
+                {
+                    using (FileStream outputStream = File.OpenWrite(outputFile))
+                    {
+                        using (BinaryWriter outputWriter = new BinaryWriter(outputStream))
+                        {
+                            for (int nSrc = 0; nSrc < srcLength; ++nSrc)
+                                //first byte
+                                if ((b = inputReader.ReadByte()) == searchByte0
+                                    || b == searchByte0Lower)
+                                {
+                                    
+                                    bytesToAdd[0] = b;
+                                    int nSearch = 1;
+                                    //next bytes
+                                    for (; nSearch < searchBytesLength; ++nSearch)
+                                        //get byte, save it and test
+                                        if ((b = bytesToAdd[nSearch] = inputReader.ReadByte()) != searchBytes[nSearch]
+                                            && b != searchBytesLower[nSearch])
+                                        {
+                                            break;//fail
+                                        }
+                                    //Avoid overflow. No need, in my case, because no chance to see searchTerm at the end.
+                                    //else if (nSrc + nSearch >= srcLength)
+                                    //    break;
+                                    
+                                    if (nSearch == searchBytesLength)
+                                    {
+                                        //success
+                                        ++counter;
+                                        outputWriter.Write(replaceBytes);
+                                        nSrc += nSearch - 1;
+                                    
+                                        
+                                    }
+                                    else
+                                    {
+                                        //failed, add saved bytes
+                                        outputWriter.Write(bytesToAdd, 0, nSearch + 1);
+                                        nSrc += nSearch;
+                                    }
+                                }
+                                else
+                                    outputWriter.Write(b);
+                        }
+                    }
+                }
+            }
+            //Console.WriteLine("ReplaceTextInBinaryFile.counter = " + counter);
+            listBox1.Items.Add("Number of matches found: " + counter);
+            //MessageBox.Show("FINISHED");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            
+            textBox1.Text = openFileDialog1.FileName;
+            textBox2.Text = openFileDialog1.FileName+".patched";
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            textBox2.Text = openFileDialog1.FileName;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
